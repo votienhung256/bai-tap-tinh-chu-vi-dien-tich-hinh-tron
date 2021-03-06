d686e8413611ed0dab8ab8ef8d5149109efe16a3
Your Name <you@example.com>
2021-03-06 19:52:13 +0700

    Bổ sung

--- chu-vi-hinh-tron.js
+++ chu-vi-hinh-tron.js
@@ -1,2 +1,3 @@
 let cv = 2 * r * 3.14;
+cv = cv.toFixed(2);
 alert(`Chu vi của hình tròn là: ${cv}.`);
--- dien-tich-hinh-tron.js
+++ dien-tich-hinh-tron.js
@@ -1,2 +1,3 @@
 let dt = r * r * 3.14;
+dt = dt.toFixed(2);
 alert(`Diện tích của hình tròn là: ${dt}.`);
